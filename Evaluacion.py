fc793e27e616a3ff0ed3227c08358653af4a01d6
locojano898 <107496060+locojano898@users.noreply.github.com>
2022-06-14 12:11:50 -0400

    Add files via upload

---
+++ Evaluacion.py
@@ -0,0 +1,7 @@
+print ("creando nuestra suma")
+a=1
+suma=0
+while a<=100:
+    suma = suma + a
+    a += 1
+print("la suma de los numeros del 1 al 100 es:",suma)
---
+++ multiplos.py
@@ -0,0 +1,4 @@
+print("los 100 primeros multiplos de 3")
+for i in range (3,100,3):
+    print(i)
+    print()
